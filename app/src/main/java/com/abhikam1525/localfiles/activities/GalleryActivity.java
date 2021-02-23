cc1cf890ff2035050136d70c47a9f9af56e099eb
abhikam395 <--add>
2021-02-23 18:56:50 +0530

    init

---
+++ app/src/main/java/com/abhikam1525/localfiles/activities/GalleryActivity.java
@@ -0,0 +1,16 @@
+package com.abhikam1525.localfiles.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.abhikam1525.localfiles.R;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+}
